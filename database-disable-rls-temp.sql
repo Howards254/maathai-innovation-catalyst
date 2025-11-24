e36c75af612205d6442fb02430acd1a4cfa0b257
howards254 <karolonyango18@gmail.com>
2025-11-24 16:18:07 +0300

    Add temporary RLS disable for testing conversation_participants

---
+++ database-disable-rls-temp.sql
@@ -0,0 +1,4 @@
+-- Temporarily disable RLS on conversation_participants to test
+-- WARNING: Only for testing, re-enable after fixing policies
+
+ALTER TABLE conversation_participants DISABLE ROW LEVEL SECURITY;
